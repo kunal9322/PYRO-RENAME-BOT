2ab8c145e7038295704ede5bf55ab20d0069ca0f
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-05-06 07:52:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID = os.environ.get("API_ID", "16743442")
 
 API_HASH = os.environ.get("API_HASH", "12bbd720f4097ba7713c5e40a11dfd2a")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5913387061:AAH6O5qVN-weLPxXCqr-FNUk3V84AiMUrBo") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5913387061:AAHU2DovYiwne8z1fUlMCLOeZ9iet_gfXbA") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "-1001813095890") 
 
@@ -14,7 +14,7 @@ DB_NAME = os.environ.get("DB_NAME","kunal")
 
 DB_URL = os.environ.get("DB_URL","mongodb+srv://Abhi12:abhi12@cluster0.bdue3ap.mongodb.net/?retryWrites=true&w=majority")
  
-FLOOD = int(os.environ.get("FLOOD", "10"))
+FLOOD = int(os.environ.get("FLOOD", "5"))
 
 START_PIC = os.environ.get("START_PIC", "https://graph.org/file/6cb6f227120d1622707ba.jpg")
 
